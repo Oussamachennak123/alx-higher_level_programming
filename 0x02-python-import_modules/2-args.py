582a947ab2ac1e5c6f7d07cbc31d79069ea17f46
Oussama <chennak.oussama@gmail.com>
2023-11-30 13:11:47 -0800

    change 2 args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__" :
+    if len(argv) == 1 :
+        print ('0 arguments.')
+    elif len(argv) == 2 :
+        print ('1 argument: ')
+    else:
+        print("{} arguments: ".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
