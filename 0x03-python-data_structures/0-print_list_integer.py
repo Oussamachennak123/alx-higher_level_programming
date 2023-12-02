e6d3ceb9350a56a87e5f43a98ddb2fbced99b2c6
Oussama <chennak.oussama@gmail.com>
2023-12-02 07:45:37 -0800

    add 0-print list integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        my_list = [1, 2, 3, 4, 5]
+        for num in my_list :
+            print('{}'.format(num))
+    print_list_integer()
