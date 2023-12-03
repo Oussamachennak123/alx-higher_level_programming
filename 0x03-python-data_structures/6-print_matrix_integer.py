b56cfe8fd0976874ace0fb91e9201941f7b5777b
Oussama <chennak.oussama@gmail.com>
2023-12-03 04:20:55 -0800

    create matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for element in x:
-            print("{:d}".format(element), end=" " if element !=x[-1] else "")
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
+        print()
