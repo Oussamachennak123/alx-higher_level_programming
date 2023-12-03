ccf71f45192a338861d7ab35012e352ab21714a5
Oussama <chennak.oussama@gmail.com>
2023-12-03 04:11:42 -0800

    create matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r in matrix :
-        for element in r :
+    for r in matrix:
+        for element in r:
             print('{:d}'.format(element), end=' ' if element !=r[-1] else '')
         print()
