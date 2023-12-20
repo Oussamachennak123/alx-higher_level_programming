bac98bd49b1af28c344eb54b53bac5700a8ed464
Oussama <chennak.oussama@gmail.com>
2023-12-20 14:19:20 -0800

    create 2 with 2 method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square():
     """ class square """
     def __init__(self, __size=0):
-        if (__size) != int:   # check if size is int
+        if type(__size) != int:   # check if size is int
             raise TypeError('size must be an integer')
         elif __size < 0:   # check if size is positive
             raise ValueError('size must be >= 0')
