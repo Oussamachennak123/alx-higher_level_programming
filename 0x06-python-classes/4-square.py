f301bb9e2dc721cd5d23e1163386ef10b244926d
Oussama <chennak.oussama@gmail.com>
2023-12-26 03:07:11 -0800

    use propertya

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" define a class Square like 0-square"""
+""" define a class Square"""
 
 
 class Square():
-    """ class square """
+    """ the class square """
     def __init__(self, __size=0):
-        if type(__size) != int:   # check if size is int
+        if type(__size) != int:
             raise TypeError("size must be an integer")
-        elif __size < 0:   # check if size is positive
+        elif __size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = __size
 
-        @property  # create the property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter  # create the setter
-        def size(self, value):
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-    def area(self):  # déclarer self comme parametre
-        return (self.__size ** 2)  # puissance 2
+    def area(self):
+        return (self.__size * self.__size)
