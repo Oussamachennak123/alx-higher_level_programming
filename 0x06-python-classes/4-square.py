6ea7ade8abcc1b05122d5ac82f35346522eefd50
Oussama <chennak.oussama@gmail.com>
2023-12-26 02:54:56 -0800

    use property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,6 +5,12 @@
 class Square():
     """ class square """
     def __init__(self, __size=0):
+        if type(__size) != int:   # check if size is int
+            raise TypeError("size must be an integer")
+        elif __size < 0:   # check if size is positive
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
 
         @property  # create the property
         def size(self):
