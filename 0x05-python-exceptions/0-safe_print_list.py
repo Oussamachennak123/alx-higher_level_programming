2d3b447196b62c198b851cf3a0a2d12bb95f49f1
Oussama <chennak.oussama@gmail.com>
2024-01-01 05:08:37 -0800

    change

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
+    """
+    safe_print_list - a function that prints <x>
+    element of <my_list>
+    """
+    Suivant = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
+            Suivant += 1
         print()
-        return my_list
     except IndexError as e:
         print(f"Erreur : {e}")
-        return my_list
+    return Suivant
