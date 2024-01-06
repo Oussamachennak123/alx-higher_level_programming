38224503e2b18c89a13119f463d06ea56e014285
Oussama <chennak.oussama@gmail.com>
2024-01-06 11:47:22 -0800

    create class with setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@
 class Rectangle:
     """ creation class Rectangle """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property  # propert width
     def width(self):
@@ -18,7 +18,7 @@ class Rectangle:
         if not isinstance(int, value):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property  # create property heighit
@@ -30,5 +30,5 @@ class Rectangle:
         if not isinstance(int, value):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
