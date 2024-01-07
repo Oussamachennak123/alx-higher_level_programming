15d0b2467201f25604b40dc7bd961ae3589a97c0
Oussama <chennak.oussama@gmail.com>
2024-01-07 11:17:54 -0800

    create 3 rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,9 +43,6 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """
-        Represents the rectangle with the # character
-        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width] * self.__heigh)
