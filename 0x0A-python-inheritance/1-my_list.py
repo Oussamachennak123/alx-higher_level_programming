b33a2bdc987469239eb886f579f8645dca5d1fb4
Oussama <chennak.oussama@gmail.com>
2024-01-09 00:53:59 +0100

    add .txt

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+A custom list class that inherits from
+the built-in list class
+"""
+
+
 class MyList(list):
-    """define class MyList that inherits from list"""
+    """ define class MyList that inherits from list """
     def print_sorted(self):
-        """define fontion print_sorted"""
+        """ define fontion print_sorted """
         print(sorted(self))
