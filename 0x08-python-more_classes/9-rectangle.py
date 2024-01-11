324eb78b62621bcb7776e604dda367b554f4cc8b
Oussama <chennak.oussama@gmail.com>
2024-01-11 19:47:11 +0100

    create def bigger or equal

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -48,19 +48,6 @@ class Rectangle:
         else:
             return 2 * (self.__width + self.__height)
 
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        line_str = [str(self.print_symbol) * self.__width]
-        return "\n".join(line_str * self.__height)
-
-    def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1  # user attribut
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the Rectangle with the greater area
@@ -78,3 +65,16 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        line_str = [str(self.print_symbol) * self.__width]
+        return "\n".join(line_str * self.__height)
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1  # user attribut
