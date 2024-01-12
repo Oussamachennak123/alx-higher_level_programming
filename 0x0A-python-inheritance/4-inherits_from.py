21b7b19be90247e99f883a0b9053378fd8fa2a01
Oussama <chennak.oussama@gmail.com>
2024-01-12 22:05:13 +0100

    change 4 inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-    Check if an object inherits from a specified class.
-
-    Parameters:
-    - obj: The object to be checked.
-    - a_class: The class to check for inheritance.
-
-    Returns:
-    True if the object inherits from the specified class, False otherwise.
-"""
+""" define def inherits_from"""
 
 
 def inherits_from(obj, a_class):
     """  that prints the list, but sorted (ascending sort) """
-    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
+    if isinstance(obj, a_class) and issubclass(type(obj), a_class):
+        return True
+    return False
