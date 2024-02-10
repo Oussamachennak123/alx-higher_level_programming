d36618b72afd4ca22714622963dfb036f296aa00
Oussama <chennak.oussama@gmail.com>
2024-02-10 21:28:57 +0100

    change json a python data structure

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""devine function returns an object(Python data structure) by a JSON"""
+import json
+
+
+def from_json_string(my_str):
+    """ user load for convered python data structure"""
+    return json.load(my_str)
