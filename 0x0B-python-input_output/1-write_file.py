c78ec3adea48084a63aac99a45e6e1f365964027
Oussama <chennak.oussama@gmail.com>
2024-02-10 20:45:54 +0100

    add fonction write the text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """ define fonction write a text with fonction write_file"""
-def write_file(file="", text=""):
-    with open(file, 'w', encoding="utf-8") as f:
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
