b3dfd776be2a7a603abc8f2b9a4845d6923a9911
Oussama <chennak.oussama@gmail.com>
2024-02-10 21:18:11 +0100

    fonction for convert objet a json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" define function returns the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """ json.dumps for chanfe objecy a json"""
+    return json.dumps(my_obj)  # change object a type json
