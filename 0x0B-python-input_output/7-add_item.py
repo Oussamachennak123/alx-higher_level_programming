20c72d88e6e2c27fa23594768d88166c73bc0936
Oussama <chennak.oussama@gmail.com>
2024-02-11 21:25:01 +0100

    create cript that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""define function that writes an Object to a text file using a JSON"""
+"""add all arguments to a python list """
 import sys
 
 
-if __name__ == "__main__":
-    """ import function save to json and load_from_jason"""
+if __name__ == "__main__": # verifier if script is principal or not
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
