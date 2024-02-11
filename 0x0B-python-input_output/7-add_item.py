7e4e0c8253c30251ec252497bb4a62a930824d2f
Oussama <chennak.oussama@gmail.com>
2024-02-11 21:21:09 +0100

    create cript that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,3 +8,10 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        m_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        m_list = []
+    m_list.extend(sys.argv[1:])
+    save_to_json_file(m_list, "add_item.json")
