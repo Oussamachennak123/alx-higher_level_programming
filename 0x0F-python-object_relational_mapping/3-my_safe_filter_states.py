85752101b8352c75dc5b2fcf257031632270b6fa
Oussama <chennak.oussama@gmail.com>
2024-04-15 23:43:01 +0100

    Script takes in an argument and displays all values in the states where `name` matches the argumentfrom the database `hbtn_0e_0_usa` This time the script is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+Script takes in an argument and displays all values in the states
+where `name` matches the argumentfrom the database `hbtn_0e_0_usa`
+This time the script is safe from MySQL injections
 """
 
 import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Access to the database and get the states
     from the database.
     """
     db_connected = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursorl = db_connected.cursor()
 
+    db_cursorl = db_connected.cursor()
     db_cursorl.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-            states.id ASC".format(argv[4]))
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
     rows_selected = db_cursorl.fetchall()
 
     for row in rows_selected:
