812ad23db93ae2d2587ddf045a7999aa30118b33
Oussama <chennak.oussama@gmail.com>
2024-04-15 01:09:30 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,14 @@ from sys import argv
 """ script that lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
-    commandDB_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, password=argv[2], db=argv[3]
-    )
+    commanddb_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    cursor_DB = commandDB_connect.cursor()  # cursor_DB objet de curseur
+    cursor_db = commanddb_connect.cursor()  # cursor_DB objet de curseur
 
-    cursor_DB.execute("SELECT * FROM states")
+    cursor_db.execute("SELECT * FROM states")
 
-    all_selected = cursor_DB.fetchall()  # affiche tous les lignes
+    all_selected = cursor_db.fetchall()  # affiche tous les lignes
 
     for row in all_selected:  # imprimer les résultats de la requête SELECT
         print(row)
