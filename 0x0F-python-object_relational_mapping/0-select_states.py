d63380ec6422234fae5bc937df0748cc2984aca3
Oussama <chennak.oussama@gmail.com>
2024-04-15 22:19:52 +0100

    change

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 """ script that lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
-    commanddb_connect = MySQLdb.connect(
+    db = commanddb_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     cursor_db = commanddb_connect.cursor()  # cursor_DB objet de curseur
@@ -16,3 +16,5 @@ if __name__ == '__main__':
 
     for row in all_selected:  # imprimer les résultats de la requête SELECT
         print(row)
+    cursor_db.close()
+    db.close()
