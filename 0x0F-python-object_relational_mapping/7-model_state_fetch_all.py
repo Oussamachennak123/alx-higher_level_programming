4dd0e212e4e4f00366aba299ce8f4b46e55abc4a
Oussama <chennak.oussama@gmail.com>
2024-04-17 00:16:54 +0100

    Script lists all State objects from the database `hbtn_0e_6_usa`

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Script lists all State objects
-from the database `hbtn_0e_6_usa`
+from the database hbtn_0e_6_usa
 """
 
 from sys import argv
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     db_urll = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create__engine(db_urll)
+    engine = create_engine(db_urll)
     Session = sessionmaker(bind=engine)
 
     session = Session()
