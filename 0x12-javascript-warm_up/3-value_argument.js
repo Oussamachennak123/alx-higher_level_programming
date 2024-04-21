53c3cfc03ae322c1c77093edf4c0d56f62391a83
Oussama <chennak.oussama@gmail.com>
2024-04-21 15:03:36 +0100

    script that prints the first argument passed to it:
    If no arguments are passed to the script, print “No argument”

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const num = process.argv[2];
+
+if (num === undefined) {
+  console.log('No argument');
+} else {
+  for (let i = 2; i < process.argv.length; i++)
+  console.log(process.argv[i]);
+}
