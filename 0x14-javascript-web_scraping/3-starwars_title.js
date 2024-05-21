78ec305be52a3bc54044c46ad9b263358b5c7c7e
Oussama <chennak.oussama@gmail.com>
2024-05-22 00:07:49 +0100

    JS

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,6 @@ const request = require('request');
 const episodeNum = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.log(err);
